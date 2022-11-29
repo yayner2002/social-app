8c6eec686b4249ed8c61d874fd3b49e4b8113ed0
yayner <yayner2002@gmail.com>
2022-11-29 18:12:42 +0300

    Add main heade

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
