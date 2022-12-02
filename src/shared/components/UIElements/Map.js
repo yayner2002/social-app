f3816f5e4a155b55a68921f1a36aed6aa68c8604
yayner <yayner2002@gmail.com>
2022-12-02 15:57:21 +0300

    Update the map using mapbox

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,27 +1,27 @@
 import React, { useRef, useEffect, useState } from "react";
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import mapboxgl from "!mapbox-gl";
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import "./Map.css";
 
+mapboxgl.accessToken = 'pk.eyJ1IjoieWF5bmVyIiwiYSI6ImNsYjY5YXp6ZzAycWwzc3BranZtNzJjMjkifQ.3Gqov8CRvDTXwaGDkiSFOQ';
+
 const Map = (props) => {
-  const { center } = props;
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const { center } = props;
   const [lng, setLng] = useState(center.lng);
   const [lat, setLat] = useState(center.lat);
   const [zoom, setZoom] = useState(props.zoom);
 
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoieWF5bmVyIiwiYSI6ImNsYjY5YXp6ZzAycWwzc3BranZtNzJjMjkifQ.3Gqov8CRvDTXwaGDkiSFOQ";
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: center,
-      zoom: zoom,
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [lng, lat],
+    zoom: zoom
     });
-  }, [center, zoom]);
+    });
+
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
@@ -40,7 +40,10 @@ const Map = (props) => {
         ref={mapContainer}
         className={`map ${props.className}`}
         style={props.style}
-      ></div>
+        id="map"
+      >
+        
+      </div>
     </>
   );
 };
