54c416470a4de44a2bfc216e8b4cfd732e456290
yayner <yayner2002@gmail.com>
2022-12-07 21:43:30 +0300

    Show a Delete warning

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -8,10 +8,8 @@ import Modal from "../../shared/components/UIElements/Modal";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
-
-  
-
   const [showMap, setShowMap] = useState(false);
+  const [showModalWarning, setShowModalWarning] = useState(false);
   return (
     <>
       <Modal
@@ -22,10 +20,36 @@ const PlaceItem = (props) => {
         footerClass="place-item__modal-actions"
         footer={<Button onClick={() => setShowMap(false)}>CLOSE</Button>}
       >
-        
         <div className="map-container">
           <Map center={props.coordinates} zoom={16} />
         </div>
+      </Modal>
+      <Modal
+        show={showModalWarning}
+        onCancel={() => showModalWarning(false)}
+        header="Are you sure ?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <>
+            <Button
+              inverse
+              onClick={() => {
+                console.log("Deleting...");
+                setShowModalWarning(false);
+              }}
+            >
+              Delete
+            </Button>
+            <Button danger onClick={() => setShowModalWarning(false)}>
+              CLOSE
+            </Button>
+          </>
+        }
+      >
+        <p>
+          Are you sure i wanted to delete this item? Please note that this can't
+          be undone thereafter.
+        </p>
       </Modal>
       <li className="place-item">
         <Card className="place-item__content">
@@ -42,7 +66,9 @@ const PlaceItem = (props) => {
               VIEW ON MAP
             </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={() => setShowModalWarning(true)}>
+              DELETE
+            </Button>
           </div>
         </Card>
       </li>
