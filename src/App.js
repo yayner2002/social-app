19da4576ee4607ec096f730e71e46e753db66a69
yayner <yayner2002@gmail.com>
2022-12-08 23:23:04 +0300

    Add auth context for app wide state mgt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 import Auth from "./user/pages/Auth";
 import Users from "./user/pages/Users";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+    }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    }, []);
+
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Users />} />
-          <Route path="/:userID/places" element={<UserPlaces />} />
-          <Route path="/places/new" element={<NewPlace />} />
-          <Route path="/places/:placeId" element={<UpdatePlace />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </main>
-    </Router>
+    <AuthContext.Provider value={
+      {
+        isLoggedIn: isLoggedIn,
+        login: login,
+        logout: logout
+      }
+    }>
+      <Router>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} />
+            <Route path="/:userID/places" element={<UserPlaces />} />
+            <Route path="/places/new" element={<NewPlace />} />
+            <Route path="/places/:placeId" element={<UpdatePlace />} />
+            <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
