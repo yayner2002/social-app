fbceffe8106ccf19f48bc3cf38b68f13ef1a27aa
yayner2002 <yayner2002@gmail.com>
2023-02-27 11:51:47 +0300

    Add Backend url and assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {useAuth} from "./shared/hooks/auth-hook";
+import React, { Suspense } from "react";
+import { useAuth } from "./shared/hooks/auth-hook";
 import {
   BrowserRouter as Router,
   Route,
@@ -7,14 +7,20 @@ import {
   Navigate,
 } from "react-router-dom";
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
+// import Users from "./user/pages/Users";
+// import NewPlace from "./places/pages/NewPlace";
+// import UserPlaces from "./places/pages/UserPlaces";
+// import UpdatePlace from "./places/pages/UpdatePlace";
+// import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
 
+const Users = React.lazy(() => import("./user/pages/Users"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 const App = () => {
   const { token, login, logout, userId } = useAuth();
@@ -51,7 +57,17 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
       </Router>
     </AuthContext.Provider>
   );
--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -8,7 +8,7 @@ const Map = () => {
     <div
       className="map"
     >
-      <iframe width="100%" title='addis ababa map' height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=Bole,%20Ring%20Road%20Airport%20Street%20Addis%20Ababa+(Addis%20Ababa)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+      <iframe width="100%" title='addis ababa map' height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=100%&amp;height=100%&amp;hl=en&amp;q=Bole,%20Ring%20Road%20Airport%20Street%20Addis%20Ababa+(Addis%20Ababa)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
 
     </div>
   );
